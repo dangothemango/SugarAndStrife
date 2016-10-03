60b090a60b47e86e0cc4f06480dbc7ca0e199eeb
Dan Gorman <danthemangorman@gmail.com>
2016-10-03 01:15:46 -0400

    first commit, includes phaser files and main.js where all the fun is gonna be

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+var game = new Phaser.Game(750,1334, Phaser.AUTO, '', { preload:preload, create: create, update:update});
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+	
+}
