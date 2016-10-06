5a8df0d0fcaed9e133d2eb298ee2ca4b646e0798
Dan Gorman <danthemangorman@gmail.com>
2016-10-06 19:53:32 -0400

    adjuste json

--- js/Ingredients.js
+++ js/Ingredients.js
@@ -3,10 +3,8 @@ var Ingredients = {
 	bleach: {
 		known=false,
 		name='Bleach',
-		attributes={
-			color=[-1,-1,-1],
-			flavor='bitter'
-			effects={ type:'poison', value=1 }
-		}
+		color=[-1,-1,-1],
+		flavor='bitter',
+		effects={ type:'poison', value=1 }
 	}
 }
