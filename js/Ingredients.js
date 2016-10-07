b1b24a0c110a93867332af47a83f96481d6a5f83
Daniel Gorman <danthemangorman@gmail.com2>
2016-10-06 20:10:00 -0400

    formatted json correctly

--- js/Ingredients.js
+++ js/Ingredients.js
@@ -1,10 +1,15 @@
 
 var Ingredients = {
 	bleach: {
-		known=false,
-		name='Bleach',
-		color=[-1,-1,-1],
-		flavor='bitter',
-		effects={ type:'poison', value=1 }
+		known: {
+			name: false,
+			color: false,
+			flavor: false,
+			effects: false
+		},
+		name: 'Bleach',
+		color: [-1,-1,-1],
+		flavor: 'bitter',
+		effects: { type:'poison', value=1 }
 	}
 }
