ca5c8945fdffdaff8df549d1a1c614f7718fc06a
Dan Gorman <danthemangorman@gmail.com>
2016-10-11 19:18:59 -0400

    added tween complete callba ck

--- js/FinalCandy.js
+++ js/FinalCandy.js
@@ -6,26 +6,30 @@ var FinalCandy= {
 	effectOrder: ['tentacles','explosive','implosion','mind_control','poison','slimification'],
 
 	createSprite: function(effects, currentColor,sparkly){
-		this.sprite=game.add.sprite(1165,515,'candy','base');
-		this.sprite.tint=PlayState.hexFromArray(currentColor);
-		this.sprite.addChild(game.add.sprite(0,0,'candy','overlay'));
-		for (var eItr = 0; eItr<this.effectOrder.length; eItr++){
-			var es = this.effectOrder[eItr];
+		FinalCandy.sprite=game.add.sprite(1165,515,'candy','base');
+		FinalCandy.sprite.tint=PlayState.hexFromArray(currentColor);
+		FinalCandy.sprite.addChild(game.add.sprite(0,0,'candy','overlay'));
+		for (var eItr = 0; eItr<FinalCandy.effectOrder.length; eItr++){
+			var es = FinalCandy.effectOrder[eItr];
 			if (effects.indexOf(es)!==-1){
-				this.sprite.addChild(game.add.sprite(0,0,'candy',es));
+				FinalCandy.sprite.addChild(game.add.sprite(0,0,'candy',es));
 			}
 		}
 		if (sparkly){
-			this.sprite.addChild(game.add.sprite(0,0,'candy','sparkly'));
+			FinalCandy.sprite.addChild(game.add.sprite(0,0,'candy','sparkly'));
 		}
-		this.sprite.anchor.set(.5,1);
-		this.sprite.scale.set(0,0);
-		for (var sC = 0; sC<this.sprite.children.length; sC++){
-			this.sprite.children[sC].anchor.set(.5,1);
+		FinalCandy.sprite.anchor.set(.5,1);
+		FinalCandy.sprite.scale.set(0,0);
+		for (var sC = 0; sC<FinalCandy.sprite.children.length; sC++){
+			FinalCandy.sprite.children[sC].anchor.set(.5,1);
 		}
 
-		game.add.tween(this.sprite.scale).to({ x: 1, y: 1}, 1250, Phaser.Easing.Quadratic.In, true, 1000);
-		game.add.tween(this.sprite).to({y:-250}, 2500, Phaser.Easing.Quadratic.In, true, 2750);
+		game.add.tween(FinalCandy.sprite.scale).to({ x: 1, y: 1}, 1250, Phaser.Easing.Quadratic.In, true, 1000);
+		game.add.tween(FinalCandy.sprite).to({y:-250}, 2500, Phaser.Easing.Quadratic.In, true, 2750).onComplete.add(FinalCandy.animFinish,game);
+	},
+
+	animFinish:function(target,tween){
+		console.log('finish');
 	}
 
 }
