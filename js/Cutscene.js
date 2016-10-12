87caa5e2e06409a68ba3911bab2c9dd2989e1460
Dan Gorman <danthemangorman@gmail.com>
2016-10-11 20:54:49 -0400

    working on cutscenes

--- js/Cutscene.js
+++ js/Cutscene.js
@@ -1,17 +1,25 @@
 
 var Cutscene = {
 
-	preload: function(){
+	scene: null,
 
+	filePath: null,
+
+	curFrame:0,
+
+	curText:null,
+
+	preload: function(){
+		game.load.spritesheet('scene','assets/images/Cutscenes/Act-1/1_1.png',1600,900,6);
 	},
 	
 	create: function() {
-
+		Cutscene.curFrame=0;
 		console.log("CutScene");
 
-		var cutsceneTextRender = game.add.text(game.world.centerX-300,game.world.height-200,
-												cutsceneText,style);
-
+		Cutscene.scene=game.add.sprite(0,0,'scene');
+		Cutscene.scene.width=game.world.width;
+		Cutscene.scene.height=game.world.height;
 		game.input.onDown.add(this.nextScreen);
 
 		
@@ -22,8 +30,13 @@ var Cutscene = {
 
 	},
 
-	nextScreen: function(){
-		game.state.start("Play");
+	nextScreen: function(z){
+		Cutscene.curFrame++;
+		if (Cutscene.curFrame<Cutscene.scene.animations.frameTotal){
+			Cutscene.scene.animations.frame=Cutscene.curFrame;
+		} else {
+			game.state.start("Play");
+		}
 	}
 
 
---
+++ js/CutsceneContent.js
@@ -0,0 +1,141 @@
+
+var CutsceneContent=[
+	
+	//Scene 0
+	{
+		open:{
+			path: 'Act-1/1_1.png',
+			text:[
+				'',
+				'I hear you\'re the person to see if I want someone \'taken care of\', and that your methods are quiet and effective.',
+				'That sounds pretty sweet to me because thatís exactly what I need.',
+				'You see, my grandfather has yet to...pass on and until then his assets are inaccessible.',
+				'It\'d be a real shame if something were to...',
+				'...happen to him.'			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-1/1_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+	//Scene 
+	{
+		open:{
+			path: 'Act-2/2_1.png',
+			text:[
+			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-2/2_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+	//Scene 
+	{
+		open:{
+			path: 'Act-3/3_1.png',
+			text:[
+			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-3/3_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+	//Scene 
+	{
+		open:{
+			path: 'Act-4/4_1.png',
+			text:[
+			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-4/4_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+	//Scene 
+	{
+		open:{
+			path: 'Act-5/5_1.pngf',
+			text:[
+			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-5/5_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+	//Scene 
+	{
+		open:{
+			path: 'Act-6/6_1.png',
+			text:[
+			
+			],
+			tCoords: [
+
+			]
+		},
+		close:{
+			path: 'Act-6/6_2.png',
+			text: [
+
+			],
+			tCoords: [
+
+			]
+		}
+	},
+
+]
