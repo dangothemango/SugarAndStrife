09188ac8a29dcc568610994a177d181596ec6014
samsuite <suitester@gmail.com>
2016-10-12 02:31:32 -0400

    music loops properly

--- js/MenuState.js
+++ js/MenuState.js
@@ -4,6 +4,7 @@ var MenuState = {
 	preload: function() {
 		game.load.atlasJSONHash('startButton', 'assets/images/buttons/play_buttons.png','assets/images/buttons/play_buttons.json');
 		game.load.image('menubg', 'assets/images/title.png');
+        game.load.audio('bgm', 'assets/sounds/backgroundMusicSkewedPaths.mp3');
 	},
 
 	
@@ -14,6 +15,9 @@ var MenuState = {
 		var startButton = game.add.button(game.world.centerX-155,game.world.centerY + 85,'startButton',startLevel,this,'Up', 'Static', 'Down', 'Static');
 		
 		startButton.scale.set(0.6,0.6);
+        bgm = game.add.audio('bgm');
+		bgm.play();
+        bgm.loopFull();
 	},
 
 	update: function () {
