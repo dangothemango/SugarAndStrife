3606b4fae84a77109c6398d0b3d6211bd405a788
Dan Gorman <danthemangorman@gmail.com>
2016-10-11 23:16:40 -0400

    acts 1-3 cutscenes

--- js/Cutscene.js
+++ js/Cutscene.js
@@ -25,9 +25,11 @@ var Cutscene = {
 		Cutscene.scene.width=game.world.width;
 		Cutscene.scene.height=game.world.height;
 		game.input.onDown.add(this.nextScreen);
-
-		Cutscene.curText=game.add.text(0,0,Cutscene.sceneData.text[Cutscene.curFrame],CSStyle);
-
+		if (Cutscene.sceneData.frames===0){
+			Cutscene.curText=game.add.text(0,0,Cutscene.sceneData.text[Cutscene.curFrame],CSStyle);
+		} else {
+			Cutscene.curText=game.add.text(Cutscene.sceneData.tCoords[Cutscene.curFrame].x,Cutscene.sceneData.tCoords[Cutscene.curFrame].y,Cutscene.sceneData.text[Cutscene.curFrame],CSStyle);
+		}
 	},
 
 	update: function() {
